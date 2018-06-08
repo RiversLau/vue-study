a416701c525453fd1494c1718bab94eb8a88c349
LiuRivers <zhaoxiang0805@hotmail.com>
2018-06-08 17:31:02 +0800

    Vue.js instance directives

---
+++ src/main/java/com/youxiang/VueStudyApplication.java
@@ -0,0 +1,12 @@
+package com.youxiang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VueStudyApplication.class, args);
+	}
+}
